792701936175aab2094cc3b94625d0dbc70234e9
Changbum97 <chb2005@naver.com>
2022-11-07 11:45:30 +0900

    Path Variable로 받은 id 화면에 출력

---
+++ src/main/java/com/mustache/springbootmustachebbs/controller/MustacheController.java
@@ -0,0 +1,28 @@
+package com.mustache.springbootmustachebbs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class MustacheController {
+
+    @GetMapping("/hi")
+    public String mustacheCon(Model model) {
+        // View에 값을 넘기겠다
+        model.addAttribute("username", "rok");
+        model.addAttribute("id", "");
+
+        // "greetings"라는 view return
+        return "greetings";
+    }
+
+    @GetMapping("/hi/{id}")
+    public String mustacheCon2(Model model, @PathVariable String id) {
+        model.addAttribute("username", "rok");
+        model.addAttribute("id", id);
+
+        return "greetings";
+    }
+}
