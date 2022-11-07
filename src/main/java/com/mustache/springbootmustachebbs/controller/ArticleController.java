ba766efe1b74c4f03886437a80ff5e8340e2bcd6
Changbum97 <chb2005@naver.com>
2022-11-07 15:15:40 +0900

    JPA Entity 생성

--- src/main/java/com/mustache/springbootmustachebbs/controller/ArticleController.java
+++ src/main/java/com/mustache/springbootmustachebbs/controller/ArticleController.java
@@ -1,6 +1,7 @@
 package com.mustache.springbootmustachebbs.controller;
 
 import com.mustache.springbootmustachebbs.domain.dto.ArticleDto;
+import com.mustache.springbootmustachebbs.domain.entity.Article;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 @Controller
 @RequestMapping("/articles")
-public class ArticlesController {
+public class ArticleController {
 
     @GetMapping("/new")
     public String newArticleForm() {
@@ -20,6 +21,10 @@ public class ArticlesController {
     @PostMapping("/posts")
     public String createArticle(ArticleDto form) {
         log.info(form.toString());
+
+        // ArticleDto를 Article로 만들어줌
+        Article article = form.toEntity();
+
         return "articles/new";
     }
 }
--- src/main/java/com/mustache/springbootmustachebbs/domain/entity/Article.java
+++ src/main/java/com/mustache/springbootmustachebbs/domain/entity/Article.java
@@ -1,2 +1,25 @@
-package com.mustache.springbootmustachebbs.domain.entity;public class Article {
+package com.mustache.springbootmustachebbs.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Article {
+    @Id @GeneratedValue
+    private Long id;
+
+    private String title;
+    private String content;
+
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
