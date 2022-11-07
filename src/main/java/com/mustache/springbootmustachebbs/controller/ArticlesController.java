5c8ab7e80325c148e0a24b406bc7117e81194edf
Changbum97 <chb2005@naver.com>
2022-11-07 14:24:28 +0900

    Mustache Form

--- src/main/java/com/mustache/springbootmustachebbs/controller/ArticlesController.java
+++ src/main/java/com/mustache/springbootmustachebbs/controller/ArticlesController.java
@@ -1,2 +1,25 @@
-package com.mustache.springbootmustachebbs.controller;public class ArticlesController {
+package com.mustache.springbootmustachebbs.controller;
+
+import com.mustache.springbootmustachebbs.domain.dto.ArticleDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/articles")
+public class ArticlesController {
+
+    @GetMapping("/new")
+    public String newArticleForm() {
+        return "articles/new";
+    }
+
+    @PostMapping("/posts")
+    public String createArticle(ArticleDto form) {
+        log.info(form.toString());
+        return "articles/new";
+    }
 }
--- src/main/java/com/mustache/springbootmustachebbs/domain/dto/ArticleDto.java
+++ src/main/java/com/mustache/springbootmustachebbs/domain/dto/ArticleDto.java
@@ -1,2 +1,14 @@
-package com.mustache.springbootmustachebbs.domain.dto;public class AtricleDto {
+package com.mustache.springbootmustachebbs.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class ArticleDto {
+    private Long id;
+    private String title;
+    private String content;
 }
