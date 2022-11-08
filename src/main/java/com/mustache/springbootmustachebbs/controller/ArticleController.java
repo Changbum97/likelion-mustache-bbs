92cceceb7114e683adaf900857ad42248c8982a3
Changbum97 <chb2005@naver.com>
2022-11-08 14:28:23 +0900

    JpaRepository를 활용한 findById

--- src/main/java/com/mustache/springbootmustachebbs/controller/ArticleController.java
+++ src/main/java/com/mustache/springbootmustachebbs/controller/ArticleController.java
@@ -6,15 +6,22 @@ import com.mustache.springbootmustachebbs.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 @Slf4j
 @Controller
 @RequestMapping("/articles")
 public class ArticleController {
 
+    // Spring이 DI 해주는 구간
+    // ArticleRepository는 interface이지만 그 구현체를 SpringBoot가 넣어줌
     private final ArticleRepository articleRepository;
     public ArticleController(ArticleRepository articleRepository) {
         this.articleRepository = articleRepository;
@@ -36,5 +43,17 @@ public class ArticleController {
         log.info(savedArticle.toString());
 
         return "articles/new";
+    }
+
+    @GetMapping("/{id}")
+    public String selectSingle(@PathVariable Long id, Model model) {
+        Optional<Article> optArticle = articleRepository.findById(id);
+
+        if(optArticle.isEmpty()) {
+            return "error";
+        }
+
+        model.addAttribute("article", optArticle.get());
+        return "articles/show";
     }
 }
