e02772ebd4e9b115bad2f392f57f81566dd06558
Changbum97 <chb2005@naver.com>
2022-11-14 14:19:40 +0900

    MySQL Query IN 사용예제

--- src/main/resources/sql/hospital.sql
+++ src/main/resources/sql/hospital.sql
@@ -8,3 +8,9 @@ SELECT *
 FROM `likelion-db`.nation_wide_hospitals
 WHERE road_name_address LIKE '서울특별시%'
 AND (business_type_name LIKE '%보건진료소%'OR business_type_name LIKE '%보건지소%' OR business_type_name LIKE '%보건소');
+
+/* 서울특별시의 보건진료소, 보건지소, 보건소 검색 - IN 사용 */
+SELECT *
+FROM `likelion-db`.nation_wide_hospitals
+WHERE road_name_address LIKE '서울특별시%'
+AND business_type_name IN ('보건진료소', '보건소', '보건지소');
