2a86404c9e2f9e600e788adca892dff40c8aab62
Changbum97 <chb2005@naver.com>
2022-11-14 13:28:12 +0900

    수원시 피부과 쿼리

--- src/main/resources/sql/hospital.sql
+++ src/main/resources/sql/hospital.sql
@@ -1,3 +1,3 @@
 SELECT *
 FROM `likelion-db`.nation_wide_hospitals
-WHERE road_name_address LIKE ''경기도 수원시%'' AND hospital_name LIKE ''%피부과%'';
+WHERE road_name_address LIKE '경기도 수원시%' AND hospital_name LIKE '%피부과%';
