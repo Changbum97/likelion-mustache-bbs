f15a9b9d32f943c8371ad2eb76f86fba5b8ac275
Changbum97 <chb2005@naver.com>
2022-11-16 11:46:32 +0900

    Article Service 추가

--- src/main/java/com/mustache/springbootmustachebbs/basicboard/service/ArticleService.java
+++ src/main/java/com/mustache/springbootmustachebbs/basicboard/service/ArticleService.java
@@ -1,7 +1,21 @@
 package com.mustache.springbootmustachebbs.basicboard.service;
 
+import com.mustache.springbootmustachebbs.basicboard.domain.dto.ArticleDto;
+import com.mustache.springbootmustachebbs.basicboard.domain.entity.Article;
+import com.mustache.springbootmustachebbs.basicboard.repository.ArticleRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+
+    public ArticleService(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
+    public ArticleDto findById(Long id) {
+        Article article = articleRepository.findById(id).get();
+        return Article.of(article);
+    }
 }
