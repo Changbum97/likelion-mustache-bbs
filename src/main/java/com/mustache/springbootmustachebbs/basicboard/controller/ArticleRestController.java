aef272e325f612b652ca3a3571c7c4119b19e26f
Changbum97 <chb2005@naver.com>
2022-11-16 11:48:19 +0900

    Article Rest Controller 작성

--- src/main/java/com/mustache/springbootmustachebbs/basicboard/controller/ArticleRestController.java
+++ src/main/java/com/mustache/springbootmustachebbs/basicboard/controller/ArticleRestController.java
@@ -1,2 +1,29 @@
-package com.mustache.springbootmustachebbs.basicboard.controller;public class ArticleRestController {
+package com.mustache.springbootmustachebbs.basicboard.controller;
+
+import com.mustache.springbootmustachebbs.basicboard.domain.dto.ArticleDto;
+import com.mustache.springbootmustachebbs.basicboard.domain.entity.Article;
+import com.mustache.springbootmustachebbs.basicboard.repository.ArticleRepository;
+import com.mustache.springbootmustachebbs.basicboard.service.ArticleService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/api/v1/articles")
+public class ArticleRestController {
+
+    private final ArticleService articleService;
+
+    public ArticleRestController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ArticleDto> getArticle(@PathVariable Long id) {
+        return ResponseEntity.ok().body(articleService.findById(id));
+    }
 }
